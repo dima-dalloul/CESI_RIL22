02453342e9bec05f345020ef28ea9e59502b144e
Dima <dima.dalloul@gmail.com>
2023-03-03 12:22:00 +0100

    Interaction entre le code et l'affchage

--- PhrasesCultes/app/src/main/java/com/cesi/phrasescultes/FirstFragment.kt
+++ PhrasesCultes/app/src/main/java/com/cesi/phrasescultes/FirstFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
+import androidx.viewbinding.ViewBindings
 import com.cesi.phrasescultes.databinding.FragmentFirstBinding
 
 /**
@@ -34,6 +37,28 @@ class FirstFragment : Fragment() {
 
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+
+        // Récupérer les phrases cultes depuis Strings.xml puis les afficher
+        var phrasesCultesListe : ArrayList<String> = arrayListOf<String>()
+        phrasesCultesListe.addAll(resources.getStringArray(R.array.phrases_cultes))
+
+        // Récupérer la référence vers le Text View créé et y afficher une phrase culte aléatoire
+        var textview = binding.phraseCulteTextview
+        textview.setText(phrasesCultesListe.random())
+
+        // Récupérer la référence vers le bouton aléatoire créé
+        // et y afficher une autre phrase culte aléatoire et changer la couleur
+        var clique = false
+        var boutonAleatoire = binding.randomPhraseButton
+        boutonAleatoire.setOnClickListener{
+            textview.setText(phrasesCultesListe.random())
+            clique = !clique
+            if(clique) {
+                boutonAleatoire.setBackgroundColor(ContextCompat.getColor(requireActivity(), R.color.bouton_aleatoire_1))
+            }  else {
+                boutonAleatoire.setBackgroundColor(ContextCompat.getColor(requireActivity(), R.color.bouton_aleatoire_2))
+            }
         }
     }
 
